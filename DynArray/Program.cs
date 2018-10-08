713fd6e41fb6d4bb4d17073fe5bbc449276310b7
Dmitriy Samofalov <samdimus@gmail.com>
2018-10-08 18:17:04 +0300

    изменен метод FindAll(), возвращаемый методом массив наполняется верным количеством найденных узлов;

---
+++ DynArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DynArray
+{
+    class DynArray
+    {
+        private int count = 0; // количество элементов в массиве
+        int capacity;  // текущая емкость буфера
+        int[] array;   // массив
+
+        DynArray()
+        {
+            capacity = 16;
+        }
+
+        public void MakeArray(int newCapacity)
+        {
+            int[] resArray = new int[newCapacity];
+            for (int i = 0; i < array.Length; i++)
+            {
+                resArray[i] = array[i];
+                array = resArray;
+            }
+        }
+
+        public int GetItem(int i)
+        {
+            try
+            {
+                return array[i];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw new IndexOutOfRangeException("Введенный индекс находится за границами массива!", e); 
+            }
+
+
+        }
+
+        public void AppEnd(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int i, int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
