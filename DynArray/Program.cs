9b1f716765a0f0eac345fb525ca6b734274fad52
Dmitriy Samofalov <samdimus@gmail.com>
2018-10-12 20:13:58 +0300

    добавлен методы: +  GetCapacity(); + Setcapacity(); + GetCount(); + Changecount(); + AppEnd(); + Insert();

--- DynArray/Program.cs
+++ DynArray/Program.cs
@@ -10,6 +10,30 @@ namespace DynArray
         private int capacity;
         object[] array;
 
+        public int GetCapacity()
+        {
+            return capacity;
+        }
+
+        public void SetCapacity(int newSize)
+        {
+            capacity = newSize;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void ChangeCount(char op)
+        {
+            if (op == '+')
+                count++;
+            else if (op == '-')
+                count--;
+        }
+
+
         DynArray()
         {
             capacity = 16;
@@ -39,9 +63,45 @@ namespace DynArray
             }
         }
 
-        public void AppEnd(object item) { }
-        public void Insert(int i, object item) { }
-        public void Delete(int i) { }
+        public void AppEnd(object item)
+        {
+            MakeArray(array.Length + 1);
+            if (array.Length >= GetCapacity())
+            {
+                SetCapacity(GetCapacity() * 2);
+            }
+
+            array[array.Length - 1] = item;
+            ChangeCount('+');
+        }
+
+        public void Insert(int index, object item)
+        {
+            object[] tempArray = new object[array.Length + 1];
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < index)
+                    tempArray[i] = array[i];
+                else if (i == index)
+                {
+                    tempArray[i] = item;
+                    tempArray[i + 1] = array[i];
+                }
+                else
+                    tempArray[i + 1] = array[i];
+            }
+            array = tempArray;
+
+            if (array.Length >= GetCapacity())
+            {
+                SetCapacity(GetCapacity() * 2);
+            }
+
+            ChangeCount('+');
+        }
+
+        public void Delete(int index) { }
 
     }
 
