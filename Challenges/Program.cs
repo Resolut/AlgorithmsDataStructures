6ba41cb4efeaacdcdc5e7e192c5370ba03aca4f7
Dmitriy Samofalov <samdimus@gmail.com>
2018-11-06 18:32:09 +0300

    Добавлен метод ConvertToStartPulse(); в методе реализовано только добавление в массив вывода минимального и максимального чисел;

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -13,20 +13,19 @@ namespace Challenges
             DateTime time = DateTime.Now;
             int seed = time.Millisecond;
             Random rand = new Random(seed);
-            // int[] resArr = new int[10]; // для тестов
-            int[] resArr = new int[rand.Next(128)];
-            while (resArr.GetLength(0) % 2 == 0)
-                resArr = new int[rand.Next(128)]; // генерируем массив произвольной длины
+            int[] resArr = new int[10]; // для тестов
+            //int[] resArr = new int[rand.Next(128)];
+            //while (resArr.GetLength(0) % 2 == 0)
+            //  resArr = new int[rand.Next(128)]; // генерируем массив случайной длины в диапазоне от 0 до 127 
+
             for (int i = 0; i < resArr.GetLength(0); i++)
             {
-                // resArr[i] = rand.Next(10);  // для тестов
-                resArr[i] = rand.Next(256);
+                resArr[i] = rand.Next(10) + 1;  // для тестов              
                 for (int j = 0; j < i; j++)
                 {
                     while (resArr[i] == resArr[j])
                     {
-                        //resArr[i] = rand.Next(10); // для тестов
-                        resArr[i] = rand.Next(256);
+                        resArr[i] = rand.Next(10) + 1; // для тестов
                         j = 0;
                     }
                 }
@@ -34,16 +33,38 @@ namespace Challenges
             return resArr;
         }
 
+        static int[] ConvertToStartPulse(int[] inArray)
+        {
+            int[] sortArr = new int[inArray.GetLength(0)];
+            int min = Int32.MaxValue;
+            int max = Int32.MinValue;
+            foreach (int item in inArray)
+            {
+                if (item < min)
+                    min = item;
+                if (item > max)
+                    max = item;
+            }
+            sortArr[0] = min;
+            sortArr[sortArr.Length - 1] = max;
+
+            return sortArr;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Генерируем массив");
-            int[] inArr = GenerateArray();
-            Console.WriteLine($"Длина массива {inArr.GetLength(0)}");
+            Console.WriteLine("Генерируем массивы");
+            //for (int i = 0; i < 20; i++)
+            //{
+                int[] inArr = ConvertToStartPulse(GenerateArray());
+                Console.WriteLine($"Длина массива {inArr.GetLength(0)}");
 
-            foreach (int item in inArr)
-            {
-                Console.Write(item + " ");
-            }
+                foreach (int item in inArr)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+           // }
 
             Console.ReadKey();
         }
