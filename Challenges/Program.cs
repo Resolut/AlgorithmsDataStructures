3c4d3c144b99d2424b51597bc617f42ba692fcfb
Dmitriy Samofalov <samdimus@gmail.com>
2018-11-07 18:51:17 +0300

    метод ConvertToStartPulse теперь выдает отсортированный массив

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -13,19 +13,19 @@ namespace Challenges
             DateTime time = DateTime.Now;
             int seed = time.Millisecond;
             Random rand = new Random(seed);
-            int[] resArr = new int[10]; // для тестов
+            int[] resArr = new int[11]; // для тестов
             //int[] resArr = new int[rand.Next(128)];
             //while (resArr.GetLength(0) % 2 == 0)
             //  resArr = new int[rand.Next(128)]; // генерируем массив случайной длины в диапазоне от 0 до 127 
 
             for (int i = 0; i < resArr.GetLength(0); i++)
             {
-                resArr[i] = rand.Next(10) + 1;  // для тестов              
+                resArr[i] = rand.Next(11) + 1;  // для тестов              
                 for (int j = 0; j < i; j++)
                 {
                     while (resArr[i] == resArr[j])
                     {
-                        resArr[i] = rand.Next(10) + 1; // для тестов
+                        resArr[i] = rand.Next(11) + 1; // для тестов
                         j = 0;
                     }
                 }
@@ -35,36 +35,57 @@ namespace Challenges
 
         static int[] ConvertToStartPulse(int[] inArray)
         {
+            // копируем массив
             int[] sortArr = new int[inArray.GetLength(0)];
-            int min = Int32.MaxValue;
-            int max = Int32.MinValue;
-            foreach (int item in inArray)
+            for (int i = 0; i < sortArr.GetLength(0); i++)
             {
-                if (item < min)
-                    min = item;
-                if (item > max)
-                    max = item;
+                sortArr[i] = inArray[i];
             }
-            sortArr[0] = min;
-            sortArr[sortArr.Length - 1] = max;
+            // сортируем массив по возрастанию
+            int max = Int32.MinValue;
+            for (int i = 0; i < sortArr.GetLength(0) - 1; i++)
+            {
+                for (int j = i + 1; j < sortArr.GetLength(0); j++)
 
+                    if (sortArr[i] > sortArr[j])
+                    {
+                        int temp = sortArr[j];
+                        sortArr[j] = sortArr[i];
+                        sortArr[i] = temp;
+                    }
+                if (sortArr[i] > max)
+                    max = sortArr[i];
+            }
+
+            int lastIndex = sortArr.GetLength(0) - 1;
+            for (int i = sortArr.GetLength(0) / 2; i < sortArr.GetLength(0) / 2 + sortArr.GetLength(0) / 4 + 1; i++)
+            {
+                int temp = sortArr[i];
+                sortArr[i] = sortArr[lastIndex];
+                sortArr[lastIndex--] = temp;
+            }
             return sortArr;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Генерируем массивы");
-            //for (int i = 0; i < 20; i++)
-            //{
-                int[] inArr = ConvertToStartPulse(GenerateArray());
-                Console.WriteLine($"Длина массива {inArr.GetLength(0)}");
 
-                foreach (int item in inArr)
-                {
-                    Console.Write(item + " ");
-                }
-                Console.WriteLine();
-           // }
+            int[] inArr = GenerateArray();
+            int[] resArr = ConvertToStartPulse(inArr);
+            Console.WriteLine($"Длина массива {inArr.GetLength(0)}");
+            Console.WriteLine("Входная последовательность");
+            foreach (int item in inArr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Стартовый имплуьс:");
+            foreach (int item in resArr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
 
             Console.ReadKey();
         }
