348f97b54d5448f91a95dc7467d984932cd3de3b
Dmitriy Samofalov <samdimus@gmail.com>
2018-11-10 17:51:36 +0300

    добавлены константы  для генератора в методе GenerateArray(); добавлен тестовый код для проверки корректности массивов добавлены  комментарии по всему коду, включая тестовый

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -13,19 +13,22 @@ namespace Challenges
             DateTime time = DateTime.Now;
             int seed = time.Millisecond;
             Random rand = new Random(seed);
-            int[] resArr = new int[11]; // для тестов
-            //int[] resArr = new int[rand.Next(128)];
-            //while (resArr.GetLength(0) % 2 == 0)
-            //  resArr = new int[rand.Next(128)]; // генерируем массив случайной длины в диапазоне от 0 до 127 
+            const int MaxSizeArray = 128;                      // Максимальное количество элементов массива 
+            const int MaxValue = 256;                          // Максимальное значение для элемента массива 
+            int[] resArr = new int[rand.Next(MaxSizeArray)];   // Генерируем массив случайной длины в диапазоне [0 - MaxSizeArray), MaxSizeArray в диапазон не входит
 
+            while (resArr.GetLength(0) % 2 == 0)
+                resArr = new int[rand.Next(MaxSizeArray)]; 
+
+            // Заполняем массив случайными значениями в диапазоне [0 - MaxValue), MaxValue в диапазон не входит
             for (int i = 0; i < resArr.GetLength(0); i++)
             {
-                resArr[i] = rand.Next(11) + 1;  // для тестов              
+                resArr[i] = rand.Next(MaxValue);
                 for (int j = 0; j < i; j++)
                 {
                     while (resArr[i] == resArr[j])
                     {
-                        resArr[i] = rand.Next(11) + 1; // для тестов
+                        resArr[i] = rand.Next(MaxValue);
                         j = 0;
                     }
                 }
@@ -35,14 +38,14 @@ namespace Challenges
 
         static int[] ConvertToStartPulse(int[] inArray)
         {
-            // копируем массив
+            // Копируем массив
             int[] sortArr = new int[inArray.GetLength(0)];
             for (int i = 0; i < sortArr.GetLength(0); i++)
             {
                 sortArr[i] = inArray[i];
             }
-            // сортируем массив по возрастанию
-            int max = Int32.MinValue;
+
+            // Сортируем весь массив по возрастанию
             for (int i = 0; i < sortArr.GetLength(0) - 1; i++)
             {
                 for (int j = i + 1; j < sortArr.GetLength(0); j++)
@@ -53,10 +56,9 @@ namespace Challenges
                         sortArr[j] = sortArr[i];
                         sortArr[i] = temp;
                     }
-                if (sortArr[i] > max)
-                    max = sortArr[i];
             }
 
+            // Сортируем вторую половину массива по убыванию
             int lastIndex = sortArr.GetLength(0) - 1;
             for (int i = sortArr.GetLength(0) / 2; i < sortArr.GetLength(0) / 2 + sortArr.GetLength(0) / 4 + 1; i++)
             {
@@ -69,23 +71,76 @@ namespace Challenges
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Генерируем массивы");
+            const int countArrays = 100;                    // Число отсортированных массивов
+            bool isFault = false;                           // Флаг ошибки
 
-            int[] inArr = GenerateArray();
-            int[] resArr = ConvertToStartPulse(inArr);
-            Console.WriteLine($"Длина массива {inArr.GetLength(0)}");
-            Console.WriteLine("Входная последовательность");
-            foreach (int item in inArr)
+            for (int count = 0; count < countArrays; count++)
             {
-                Console.Write(item + " ");
+                int[] inArr = GenerateArray();              // Входной массив
+                int[] testArr = ConvertToStartPulse(inArr); // Отсортированный массив
+                int max = Int32.MinValue;
+                int min = Int32.MaxValue;
+
+                for (int i = 0; i < testArr.Length; i++)
+                {
+                    // Массив с одним элементом является отсортированным
+                    if (testArr.Length > 1)
+                    {
+                        // Проверка на возрастание первой половины массива: 
+                        // каждый элемент первой половины, начиная с элемента с индексом = 1 больше предыдущего
+                        if (i > 0 && i < testArr.Length / 2)
+                        {
+                            if (testArr[i] <= testArr[i - 1])
+                            {
+                                isFault = true;
+                                Console.WriteLine("Ошибка 001! Первая половина массива сформирована некорректно!");
+                                break;
+                            }
+
+                        }
+
+                        // Проверка на убывание второй половины массива: 
+                        // каждый элемент второй половины меньше предыдущего
+                        if (i > testArr.Length / 2)
+                        {
+                            if (testArr[i] >= testArr[i - 1])
+                            {
+                                isFault = true;
+                                Console.WriteLine("Ошибка 002! Вторая половина массива сформирована некорректно!");
+                                break;
+                            }
+
+                        }
+
+                        // Проверка: наименьший элемент второй половины больше любого элемента из первой половины масива 
+                        if (i == testArr.Length - 1 && testArr[i] <= testArr[testArr.Length / 2 - 1])
+                        {
+                            isFault = true;
+                            Console.WriteLine("Ошибка 003! Массив отсоритрован некоректно!");
+                        }
+                    }
+
+                    // Определение максимального и минимального элементов
+                    if (testArr[i] > max)
+                        max = testArr[i];
+                    if (testArr[i] < min)
+                        min = testArr[i];
+                }
+
+                // Проверка расположения максимального и минимального элементов
+                if (max != testArr[testArr.Length / 2] || min != testArr[0])
+                {
+                    isFault = true;
+                    Console.WriteLine("Ошибка 004! Минимум и максимум расставлены некорректно!");
+                }
+
             }
-            Console.WriteLine();
-            Console.WriteLine("Стартовый имплуьс:");
-            foreach (int item in resArr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+            Console.WriteLine($"Общее количество тестов: {countArrays}");
+
+            if (isFault)
+                Console.WriteLine("НЕУДАЧА! Двигатель работает не в полную силу!");
+            else
+                Console.WriteLine("УСПЕХ! Двигатель работает на максимальных оборотах!");
 
             Console.ReadKey();
         }
