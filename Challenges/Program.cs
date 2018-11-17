9d7b6ba29db4b77b6ab3e1d9728fc51987693918
Dmitriy Samofalov <samdimus@gmail.com>
2018-11-17 14:31:31 +0300

    временное изменение кода для проверки генерации неповторяющихся 20 элементов массива

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -14,23 +14,30 @@ namespace Challenges
             DateTime time = DateTime.Now;
             int seed = time.Millisecond;
             Random rand = new Random(seed);
-            const int MaxSizeArray = 128;                      // Максимальное количество элементов массива 
-            const int MaxValue = 256;                          // Максимальное значение для элемента массива 
+            //const int MaxSizeArray = 128;                      // Максимальное количество элементов массива 
+            const int MaxSizeArray = 20;                      // Максимальное количество элементов массива (тест)
+            //const int MaxValue = 256;                          // Максимальное значение для элемента массива 
+            const int MaxValue = 20;                          // Максимальное значение для элемента массива (тест)
 
             // Генерируем массив случайной длины в диапазоне [0 : MaxSizeArray), MaxSizeArray в диапазон не входит
-            int[] resArr = new int[rand.Next(MaxSizeArray)];
+            //int[] resArr = new int[rand.Next(MaxSizeArray)];
+
+            // Для теста массив длиной 20
+            int[] resArr = new int[MaxSizeArray];
 
             // Формируем непустой массив нечётной длины
-            while (resArr.GetLength(0) % 2 == 0)
-                resArr = new int[rand.Next(MaxSizeArray)];
+            //while (resArr.GetLength(0) % 2 == 0)
+            //    resArr = new int[rand.Next(MaxSizeArray)];
 
             // Заполняем массив случайными значениями в диапазоне [0 - MaxValue), MaxValue в диапазон не входит
             for (int i = 0; i < resArr.GetLength(0); i++)
             {
-                resArr[i] = rand.Next(MaxValue);
+                //resArr[i] = rand.Next(MaxValue);
+                resArr[i] = 16;
                 for (int j = 0; j < i; j++)
                 {
-                    while (resArr[i] == resArr[j])
+                    //while (resArr[i] == resArr[j])
+                    if (resArr[i] == resArr[j])
                     {
                         resArr[i] = rand.Next(MaxValue);
                         j = 0;
@@ -79,76 +86,84 @@ namespace Challenges
 
         static void Main(string[] args)
         {
-            const int countArrays = 100;                    // Число отсортированных массивов
-            bool isFault = false;                           // Флаг ошибки
+            // const int countArrays = 100;                    // Число отсортированных массивов
+            const int countArrays = 20;                    // Число отсортированных массивов (Тест)
+            // bool isFault = false;                           // Флаг ошибки
 
             for (int count = 0; count < countArrays; count++)
             {
                 int[] inArr = GenerateArray();              // Входной массив
-                int[] testArr = ConvertToStartPulse(inArr); // Отсортированный массив
-                int max = Int32.MinValue;
-                int min = Int32.MaxValue;
-
-                for (int i = 0; i < testArr.Length; i++)
+                // Console.WriteLine($"Длина массива: {inArr.Length}"); // Для теста
+                foreach (int item in inArr)
                 {
-                    // Массив с одним элементом является отсортированным
-                    if (testArr.Length > 1)
-                    {
-                        // Проверка на возрастание первой половины массива: 
-                        // каждый элемент первой половины, начиная с элемента с индексом = 1 больше предыдущего
-                        if (i > 0 && i < testArr.Length / 2)
-                        {
-                            if (testArr[i] <= testArr[i - 1])
-                            {
-                                isFault = true;
-                                Console.WriteLine("Ошибка 001! Первая половина массива сформирована некорректно!");
-                                break;
-                            }
-
-                        }
-
-                        // Проверка на убывание второй половины массива: 
-                        // каждый элемент второй половины меньше предыдущего
-                        if (i > testArr.Length / 2)
-                        {
-                            if (testArr[i] >= testArr[i - 1])
-                            {
-                                isFault = true;
-                                Console.WriteLine("Ошибка 002! Вторая половина массива сформирована некорректно!");
-                                break;
-                            }
-
-                        }
-
-                        // Проверка: наименьший элемент второй половины больше любого элемента из первой половины масива 
-                        if (i == testArr.Length - 1 && testArr[i] <= testArr[testArr.Length / 2 - 1])
-                        {
-                            isFault = true;
-                            Console.WriteLine("Ошибка 003! Массив отсоритрован некоректно!");
-                        }
-                    }
-
-                    // Определение максимального и минимального элементов
-                    if (testArr[i] > max)
-                        max = testArr[i];
-                    if (testArr[i] < min)
-                        min = testArr[i];
+                    Console.Write($"{item} ");
                 }
+                Console.WriteLine();
 
-                // Проверка расположения максимального и минимального элементов
-                if (max != testArr[testArr.Length / 2] || min != testArr[0])
-                {
-                    isFault = true;
-                    Console.WriteLine("Ошибка 004! Минимум и максимум расставлены некорректно!");
-                }
+                //int[] testArr = ConvertToStartPulse(inArr); // Отсортированный массив
+                //int max = Int32.MinValue;
+                //int min = Int32.MaxValue;
+
+                //for (int i = 0; i < testArr.Length; i++)
+                //{
+                //    // Массив с одним элементом является отсортированным
+                //    if (testArr.Length > 1)
+                //    {
+                //        // Проверка на возрастание первой половины массива: 
+                //        // каждый элемент первой половины, начиная с элемента с индексом = 1 больше предыдущего
+                //        if (i > 0 && i < testArr.Length / 2)
+                //        {
+                //            if (testArr[i] <= testArr[i - 1])
+                //            {
+                //                isFault = true;
+                //                Console.WriteLine("Ошибка 001! Первая половина массива сформирована некорректно!");
+                //                break;
+                //            }
+
+                //        }
+
+                //        // Проверка на убывание второй половины массива: 
+                //        // каждый элемент второй половины меньше предыдущего
+                //        if (i > testArr.Length / 2)
+                //        {
+                //            if (testArr[i] >= testArr[i - 1])
+                //            {
+                //                isFault = true;
+                //                Console.WriteLine("Ошибка 002! Вторая половина массива сформирована некорректно!");
+                //                break;
+                //            }
+
+                //        }
+
+                //        // Проверка: наименьший элемент второй половины больше любого элемента из первой половины масива 
+                //        if (i == testArr.Length - 1 && testArr[i] <= testArr[testArr.Length / 2 - 1])
+                //        {
+                //            isFault = true;
+                //            Console.WriteLine("Ошибка 003! Массив отсоритрован некоректно!");
+                //        }
+                //    }
+
+                //    // Определение максимального и минимального элементов
+                //    if (testArr[i] > max)
+                //        max = testArr[i];
+                //    if (testArr[i] < min)
+                //        min = testArr[i];
+                //}
+
+                //// Проверка расположения максимального и минимального элементов
+                //if (max != testArr[testArr.Length / 2] || min != testArr[0])
+                //{
+                //    isFault = true;
+                //    Console.WriteLine("Ошибка 004! Минимум и максимум расставлены некорректно!");
+                //}
             }
 
-            Console.WriteLine($"Общее количество тестов: {countArrays}");
+            //Console.WriteLine($"Общее количество тестов: {countArrays}");
 
-            if (isFault)
-                Console.WriteLine("НЕУДАЧА! Двигатель работает не в полную силу!");
-            else
-                Console.WriteLine("УСПЕХ! Двигатель работает на максимальных оборотах!");
+            //if (isFault)
+            //    Console.WriteLine("НЕУДАЧА! Двигатель работает не в полную силу!");
+            //else
+            //    Console.WriteLine("УСПЕХ! Двигатель работает на максимальных оборотах!");
 
             Console.ReadKey();
         }
