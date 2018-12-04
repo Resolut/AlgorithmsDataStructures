19bac969753f508c388581d5343f4adb8157e96d
Dmitriy Samofalov <samdimus@gmail.com>
2018-12-04 17:30:51 +0300

    Добавлен файл MainProgram.cs Изменен метод RemoveAll();

--- LinkedList/MainProgram.cs
+++ LinkedList/MainProgram.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace AlgorithmsDataStructures
+namespace LinkedList
 {
-
     public class Node
     {
         public int value;
@@ -167,5 +169,13 @@ namespace AlgorithmsDataStructures
             return res;
         }
 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
