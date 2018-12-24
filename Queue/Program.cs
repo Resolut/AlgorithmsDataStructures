7ee78580958536e305ef9f93f359ad9248690887
Dmitriy Samofalov <samdimus@gmail.com>
2018-12-24 18:47:16 +0300

    В проект добавлен класс Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Queue
+{
+    public class Queue<T>
+    {
+        LinkedList<T> items;
+
+        public Queue()
+        {
+            items = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            items.AddFirst(item);
+        }
+
+        public T Dequeue()
+        {
+            T lastItem = default(T);
+            if (Size() > 0)
+            {
+                lastItem = items.Last.Value;
+                items.RemoveLast();
+
+                return lastItem;
+            }
+            return default(T);
+        }
+
+        public int Size()
+        {
+            return items.Count;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
