cea8d0abd2311f4712f4bf257d5499d425dee814
Dmitriy Samofalov <samdimus@gmail.com>
2018-12-29 18:46:49 +0300

    Добавлен класс Deque

---
+++ Dequeue/AutotestProgram.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    class Deque<T>
+    {
+        LinkedList<T> items;
+
+        public Deque()
+        {
+            // инициализация внутреннего хранилища
+            items = new LinkedList<T>();
+        }
+
+        public void addFront(T item)
+        {
+            // добавление в голову
+            items.AddLast(item);
+        }
+
+        public void addTail(T item)
+        {
+            // добавление в хвост
+            items.AddFirst(item);
+        }
+
+        public T removeFront()
+        {
+            // удаление из головы
+            T lastItem = default(T);
+
+            if (size() > 0)
+            {
+                lastItem = items.Last.Value;
+                items.RemoveLast();
+
+                return lastItem;
+            }
+
+            return default(T);
+        }
+
+        public T removeTail()
+        {
+            // удаление из хвоста
+            T firstItem = default(T);
+
+            if (size() > 0)
+            {
+                firstItem = items.First.Value;
+                items.RemoveFirst();
+
+                return firstItem;
+            }
+
+            return default(T);
+        }
+
+        public int size()
+        {   // размер очереди
+            return items.Count; 
+        }
+    }
+
+}
---
+++ Dequeue/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dequeue
+{
+    class Deque<T>
+    {
+        LinkedList<T> items;
+
+        public Deque()
+        {
+            items = new LinkedList<T>();
+        }
+
+        public void AddFront(T item)
+        {
+            items.AddLast(item);
+        }
+
+        public void AddTail(T item)
+        {
+            items.AddFirst(item);
+        }
+
+        public T RemoveFront()
+        {
+            T lastItem = default(T);
+
+            if (Size() > 0)
+            {
+                lastItem = items.Last.Value;
+                items.RemoveLast();
+
+                return lastItem;
+            }
+
+            return default(T);
+        }
+
+        public T RemoveTail()
+        {
+            T firstItem = default(T);
+
+            if (Size() > 0)
+            {
+                firstItem = items.First.Value;
+                items.RemoveFirst();
+
+                return firstItem;
+            }
+
+            return default(T);
+        }
+
+        public int Size()
+        {
+            return items.Count; 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque<int> testDeq = new Deque<int>();
+            testDeq.AddFront(1);
+            testDeq.AddFront(2);
+            testDeq.AddFront(3);
+            testDeq.AddFront(4);
+            
+            for (int i = 0; i < testDeq.Size(); i++ ) {
+                Console.Write("{0} ", testDeq.RemoveTail());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
