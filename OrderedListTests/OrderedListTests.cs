9c8482b173ef373540f83fe71f83254e42667cae
Dmitriy Samofalov <samdimus@gmail.com>
2019-01-25 19:00:06 +0300

    Добавлен шаблон для тестов методов класса OrderedList

---
+++ OrderedListTests/OrderedListTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures.Tests
+{
+    [TestClass()]
+    public class OrderedListTests
+    {
+        [TestMethod()]
+        public void Add_Empty_Asc_List()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void Add_Empty_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Add_in_between_nodes_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Add_in_between_nodes_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Add_if_More_than_the_Tail_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Add_if_Less_than_the_Head_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Add_if_Less_than_the_Tail_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Add_if_More_than_the_Head_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Find_in_Empty_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Find_in_Empty_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Find_if_Elem_Is_not_Exists_in_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Find_if_Elem_Is_not_Exists_in_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Find_if_Elem_Is_Exists_in_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Find_if_Elem_Is_Exists_in_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Delete_from_Empty_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Delete_from_Empty_Desc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Delete_if_Elem_Is_Exists_in_Asc_List()
+        {
+
+        }
+
+        [TestMethod()]
+        public void Delete_if_Elem_Is_Exists_in_Desc_List()
+        {
+
+        }
+    }
+}
