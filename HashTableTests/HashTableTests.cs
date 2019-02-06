9bf43fd36a9fc0aa81daef2bb69e20f9b718c09e
Dmitriy Samofalov <samdimus@gmail.com>
2019-02-06 09:32:35 +0300

    Удален неиспользуемая директива using

--- HashTableTests/HashTableTests.cs
+++ HashTableTests/HashTableTests.cs
@@ -1,5 +1,4 @@
-﻿using AlgorithmsDataStructures;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AlgorithmsDataStructures.Tests
 {
