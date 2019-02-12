db8e3af509737380bdec569efdd6e8b4871faa33
Dmitriy Samofalov <samdimus@gmail.com>
2019-02-12 21:22:30 +0300

    Добавлены 2 хэш-функции в конструктор класса HashTable

--- HasTable/Program.cs
+++ HasTable/Program.cs
@@ -7,9 +7,11 @@ namespace HashTable
         public int size;
         public int step;
         public string[] slots;
-        private int currentHash;
         private delegate int HashF(string value);
+
         List<HashF> familyHashFun = new List<HashF>();
+
+        public int CurrentHash { get; set; } // случайно выбранная хэш-функции 
 
         public HashTable(int sz, int stp)
         {
@@ -30,19 +32,54 @@ namespace HashTable
                     hash = ((a * hash + b) % p) % size;
                 }
                 return hash;
-            };  
+            };
 
-            HashF func2 = (value) => { return value.Length; }; // TODO Добавить код хэш-функции 2
-            HashF func3 = (value) => { return value.Length; }; // TODO добавить код хэш-функции 3
+            // Хэш-функция 2
+            HashF func2 = (value) =>
+            {
+                int hash = 0;
+                int a = 8;
+                int b = 11;
+                int p = 17;
+
+                if (value != null)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        hash += value[i];
+                    }
+                    hash = ((a * hash + b) % p) % size;
+                }
+                return hash;
+            };
+
+            // Хэш-функция 3
+            HashF func3 = (value) =>
+            {
+                int hash = 0;
+                int a = 7;
+                int b = 0;
+                int p = 13;
+
+                if (value != null)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        hash += value[i];
+                    }
+                    hash = ((a * hash + b) % p) % size;
+                }
+                return hash;
+            };
 
             familyHashFun.Add(func1);
             familyHashFun.Add(func2);
             familyHashFun.Add(func3);
-            
+
             System.DateTime time = System.DateTime.Now;
             int seed = time.Millisecond;
             System.Random rand = new System.Random(seed);
-            currentHash = rand.Next(familyHashFun.Count); // Случайный выбор хэш-функции из списка
+            CurrentHash = rand.Next(familyHashFun.Count); // Случайный выбор хэш-функции из списка
 
             size = sz;
             step = stp;
@@ -53,7 +90,7 @@ namespace HashTable
 
         public int HashFun(string value)
         {
-            return familyHashFun[currentHash](value); // Вызываеем хэш-функцию, определённую случайным образом в конструкторе
+            return familyHashFun[CurrentHash](value); // Вызываеем хэш-функцию, определённую случайным образом в конструкторе
         }
 
         public int SeekSlot(string value)
@@ -128,6 +165,17 @@ namespace HashTable
 
             return -1; // не удалось найти свободный слот из-за коллизий
         }
+
+        public void PrintHashTable()
+        {
+            for (int i = 0; i < slots.Length; i++)
+            {
+                if (slots[i] == null)
+                    System.Console.WriteLine(@"slot [{0}] null", i);
+                else
+                    System.Console.WriteLine(@"slot [{0}] = {1}", i, slots[i]);
+            }
+        }
     }
 
 
@@ -135,6 +183,28 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
+            HashTable table = new HashTable(23, 5);
+            HashTable table2 = new HashTable(23, 5);
+            HashTable table3 = new HashTable(23, 5);
+
+            for (int i = 0; i < table.size; i++)
+            {
+                string value = "" + (char)(i + 33);
+                table.Put(value);
+                table2.Put(value);
+                table3.Put(value);
+            }
+
+            System.Console.WriteLine("Таблица 1 используемая хэш-функция #{0}", table.CurrentHash + 1);
+            table.PrintHashTable();
+            System.Console.WriteLine("Таблица 2 используемая хэш-функция #{0}", table2.CurrentHash + 1);
+            table2.PrintHashTable();
+            System.Console.WriteLine("Таблица 3 используемая хэш-функция #{0}", table3.CurrentHash + 1);
+            table3.PrintHashTable();
+
+            System.Console.ReadKey();
         }
+
+
     }
 }
