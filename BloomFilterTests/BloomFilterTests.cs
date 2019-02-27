926f90cffc7eac7a0fc6d4bc9a31b0dc6adc3b77
Dmitriy Samofalov <samdimus@gmail.com>
2019-02-27 15:29:57 +0300

    добавлен тест на ложноположительное срабатывание фильтра

---
+++ BloomFilterTests/BloomFilterTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsDataStructures.Tests
+{
+    [TestClass()]
+    public class BloomFilterTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            BloomFilter testFilter = new BloomFilter(32);
+            string s1 = "0123456789";
+            string s2 = "1234567890";
+            string s3 = "2345678901";
+            string s4 = "3456789012";
+            string s5 = "4567890123";
+            string s6 = "5678901234";
+            string s7 = "6789012345";
+            string s8 = "7890123456";
+            string s9 = "8901234567";
+            string s10 = "9012345678";
+
+            testFilter.Add(s1);
+
+            Assert.IsTrue(testFilter.IsValue(s1)); // добавленный элемент присутствует
+            Assert.IsFalse(testFilter.IsValue(s2));
+            Assert.IsFalse(testFilter.IsValue(s3));
+            Assert.IsFalse(testFilter.IsValue(s4));
+            Assert.IsFalse(testFilter.IsValue(s5));
+            Assert.IsFalse(testFilter.IsValue(s6));
+            Assert.IsFalse(testFilter.IsValue(s7));
+            Assert.IsFalse(testFilter.IsValue(s8));
+            Assert.IsTrue(testFilter.IsValue(s9)); // ложноположительное срабатаывание
+            Assert.IsFalse(testFilter.IsValue(s10));
+        }
+    }
+}
