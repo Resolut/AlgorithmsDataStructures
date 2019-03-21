b320ec9c4c92edec7bb7fe8b18bdd18daa25d0b7
Dmitriy Samofalov <samdimus@gmail.com>
2019-03-21 18:40:42 +0300

    добавлен тест проверки удаления несуществующего узла

--- SimpleTreeTests/SimpleTreeTests.cs
+++ SimpleTreeTests/SimpleTreeTests.cs
@@ -172,7 +172,7 @@ namespace AlgorithmsDataStructures2.Tests
 
             foreach (var item in foundtList)
             {
-                Console.WriteLine("item = {0}", item.NodeValue); 
+                Console.WriteLine("item = {0}", item.NodeValue);
             }
             Assert.IsTrue(foundtList.Count == 1);
             Assert.IsTrue(testTree.Count() == 6);
@@ -182,7 +182,7 @@ namespace AlgorithmsDataStructures2.Tests
         public void Get_FoundNodes_If_Duplicates_Exists()
         {
             SimpleTree<int> testTree = new SimpleTree<int>(new SimpleTreeNode<int>(1, null));
-            
+
             testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(2, null)); // узлы первого уровня
             testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(3, null));
             testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(4, null));
@@ -212,7 +212,7 @@ namespace AlgorithmsDataStructures2.Tests
             int actualFoundNodes = foundtList.Count;
 
             Assert.AreEqual(expectedCount, actualCount);
-            Assert.AreEqual(expectedLeafs,actualLeafs);
+            Assert.AreEqual(expectedLeafs, actualLeafs);
             Assert.AreEqual(expectedFoundNodes, actualFoundNodes);
         }
 
@@ -235,13 +235,13 @@ namespace AlgorithmsDataStructures2.Tests
             testTree.AddChild(testTree.Root.Children[4].Children[0], new SimpleTreeNode<int>(7, null)); // узлы третьего уровня
 
             List<SimpleTreeNode<int>> foundList = testTree.FindNodesByValue(12); // Список найденных узлов
-            
+
             int expectedCount = 11;
             int expectedLeafs = 5;
-            
+
             int actualCount = testTree.Count();
             int actualLeafs = testTree.LeafCount();
-            
+
             Assert.AreEqual(expectedCount, actualCount);
             Assert.AreEqual(expectedLeafs, actualLeafs);
             Assert.IsNull(foundList);
@@ -264,7 +264,7 @@ namespace AlgorithmsDataStructures2.Tests
             testTree.AddChild(testTree.Root.Children[4], new SimpleTreeNode<int>(8, null));
 
             testTree.AddChild(testTree.Root.Children[4].Children[0], new SimpleTreeNode<int>(7, null)); // узлы третьего уровня
-                        
+
             int beforeDelNodesCount = 11;
             int beforeDelLeafs = 5;
 
@@ -274,7 +274,7 @@ namespace AlgorithmsDataStructures2.Tests
             Assert.AreEqual(beforeDelNodesCount, actualNodesCount);
             Assert.AreEqual(beforeDelLeafs, actualLeafs);
 
-            testTree.DeleteNode(testTree.Root.Children[3]); // удаление листа
+            testTree.DeleteNode(testTree.Root.Children[3]); // удаление листа со значением "5"
 
             int afterDelNodesCount = 10;
             int afterDelLeafs = 4;
@@ -287,7 +287,7 @@ namespace AlgorithmsDataStructures2.Tests
             Assert.IsNull(testTree.FindNodesByValue(5));
             Assert.IsTrue(testTree.Root.Children.Count == 4);
 
-            testTree.DeleteNode(testTree.Root.Children[2].Children[0]); // удаление вначале потомка-листа, затем его родительского узла
+            testTree.DeleteNode(testTree.Root.Children[2].Children[0]); // удаление вначале потомка-листа ("7"), затем его родительского узла ("4")
             testTree.DeleteNode(testTree.Root.Children[2]);
 
             Assert.IsNull(testTree.FindNodesByValue(4));
@@ -295,9 +295,9 @@ namespace AlgorithmsDataStructures2.Tests
             Assert.IsTrue(testTree.Count() == 8);
 
             Console.WriteLine("=========Log==========");
-            Console.Write("Корень: {0}", testTree.Root.NodeValue);
-            Console.Write("\t Узлов: {0}", testTree.Count());
-            Console.WriteLine("\t Листьев: {0}", testTree.LeafCount());
+            Console.WriteLine("Корень: {0}", testTree.Root.NodeValue);
+            Console.WriteLine("Всего Узлов: {0}", testTree.Count());
+            Console.WriteLine("Всего Листьев: {0}", testTree.LeafCount());
 
             List<SimpleTreeNode<int>> nodes = testTree.GetAllNodes();
 
@@ -315,6 +315,73 @@ namespace AlgorithmsDataStructures2.Tests
                 }
                 Console.WriteLine();
             }
+        }
+
+        [TestMethod()]
+        public void DeleteNode_If_Node_IS_NOT_Exists()
+        {
+            SimpleTree<int> testTree = new SimpleTree<int>(new SimpleTreeNode<int>(1, null));
+
+            testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(2, null)); // узлы первого уровня
+            testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(3, null));
+            testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(4, null));
+            testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(5, null));
+            testTree.AddChild(testTree.Root, new SimpleTreeNode<int>(6, null));
+
+            testTree.AddChild(testTree.Root.Children[0], new SimpleTreeNode<int>(7, null)); // узлы второго уровня
+            testTree.AddChild(testTree.Root.Children[1], new SimpleTreeNode<int>(7, null));
+            testTree.AddChild(testTree.Root.Children[2], new SimpleTreeNode<int>(7, null));
+            testTree.AddChild(testTree.Root.Children[4], new SimpleTreeNode<int>(8, null));
+
+            testTree.AddChild(testTree.Root.Children[4].Children[0], new SimpleTreeNode<int>(7, null)); // узлы третьего уровня
+
+            int beforeDelNodesCount = 11;
+            int beforeDelLeafs = 5;
+
+            int actualNodesCount = testTree.Count();
+            int actualLeafs = testTree.LeafCount();
+
+            Assert.AreEqual(beforeDelNodesCount, actualNodesCount);
+            Assert.AreEqual(beforeDelLeafs, actualLeafs);
+
+            testTree.DeleteNode(new SimpleTreeNode<int>(7, testTree.Root.Children[4].Children[0])); // попытка удаления псевдо-листа со значением "7"
+
+            int afterDelNodesCount = 11;
+            int afterDelLeafs = 5;
+
+            int actualAfterDelNodesCount = testTree.Count();
+            int actualAfterDelLeafs = testTree.LeafCount();
+
+            Console.WriteLine("=========Log==========");
+            Console.WriteLine("Всего Узлов: {0}", testTree.Count());
+            Console.WriteLine("Всего Листьев: {0}\n", testTree.LeafCount());
+
+            List<SimpleTreeNode<int>> nodes = testTree.GetAllNodes();
+
+            foreach (SimpleTreeNode<int> node in nodes)
+            {
+                if (node == testTree.Root)
+                    Console.WriteLine("Корень дерева:\t{0}\nРодитель: -", node.NodeValue);
+                else
+                    Console.WriteLine("Текущий узел:\t{0}\nРодитель:\t{1}", node.NodeValue, node.Parent.NodeValue);
+
+                Console.Write("Потомки:");
+                for (int i = 0; node.Children != null && i < node.Children.Count; i++)
+                {
+                    Console.Write(" [{0} ", node.Children[i].NodeValue);
+                    if (node.Children[i].Children == null)
+                        Console.Write(" - нет своих потомков; ]");
+                    else
+                        Console.Write(" - Есть свои потомки; ]");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Assert.AreEqual(afterDelNodesCount, actualAfterDelNodesCount);
+            Assert.IsTrue(testTree.Root.Children[4].Children[0].Children.Count == 1);
+            Assert.AreEqual(afterDelLeafs, actualAfterDelLeafs);
+
         }
     }
 }
