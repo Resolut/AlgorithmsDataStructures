766078776cae5c9f4ec7a55e10d740395c7bc6bd
Dmitriy Samofalov <samdimus@gmail.com>
2019-04-15 18:46:16 +0300

    добавлен код метода FindKeyIndex добавлен расчет величины массива для заданной глубины дерева

--- ABSTree/AutotestProgram.cs
+++ ABSTree/AutotestProgram.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures2
 {
@@ -10,7 +9,7 @@ namespace AlgorithmsDataStructures2
         public aBST(int depth)
         {
             // правильно рассчитайте размер массива для дерева глубины depth:
-            int tree_size = 0;
+            int tree_size = (int)Math.Pow(2, depth) - 1;
             Tree = new int?[tree_size];
             for (int i = 0; i < tree_size; i++) Tree[i] = null;
         }
@@ -18,7 +17,29 @@ namespace AlgorithmsDataStructures2
         public int? FindKeyIndex(int key)
         {
             // ищем в массиве индекс ключа
-            return null; // не найден
+            if (Tree[0] == null) return null;
+            int? currentKey = Tree[0];
+            int index = 0;
+
+            while (true)
+            {
+                if (key == currentKey) return index; // ключ найден
+
+                if (key < currentKey)
+                    index = 2 * index + 1; // двигаемся влево
+                else
+                    index = 2 * index + 2; // двигаемся вправо
+                if (index < Tree.Length)  // проверка, не превышена ли макс. глубина дерева
+                {
+                    currentKey = Tree[index];
+
+                    if (currentKey == null)
+                        return -index; // индекс незаполненного слота
+                }
+                else break; // глубина превышена
+            }
+
+            return null; // ключ не найден, дерево пройдено до макс. глубины
         }
 
         public int AddKey(int key)
