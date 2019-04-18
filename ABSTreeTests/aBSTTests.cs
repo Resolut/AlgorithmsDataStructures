27637e9b8cf06e1b32d4ffca64e2cdca56bd4265
Dmitriy Samofalov <samdimus@gmail.com>
2019-04-18 09:57:09 +0300

    добавлен тест поиска, когда все свободные слоты в последнем уровне дерева

--- ABSTreeTests/aBSTTests.cs
+++ ABSTreeTests/aBSTTests.cs
@@ -130,21 +130,21 @@ namespace AlgorithmsDataStructures2.Tests
             int indexFull = testTree.AddKey(0);
             int indexDuplicate = testTree.AddKey(7);
 
-            int expectedIndex1 = 0;
-            int expectedIndex2 = 1;
-            int expectedIndex3 = 2;
-            int expectedIndex4 = 3;
-            int expectedIndex5 = 4;
-            int expectedIndex6 = 5;
-            int expectedIndex7 = 6;
-            int expectedIndex8 = 7;
-            int expectedIndex9 = 8;
-            int expectedIndex10 = 9;
-            int expectedIndex11 = 10;
-            int expectedIndex12 = 11;
-            int expectedIndex13 = 12;
-            int expectedIndex14 = 13;
-            int expectedIndex15 = 14;
+            int expectedAddedIndex1 = 0;
+            int expectedAddedIndex2 = 1;
+            int expectedAddedIndex3 = 2;
+            int expectedAddedIndex4 = 3;
+            int expectedAddedIndex5 = 4;
+            int expectedAddedIndex6 = 5;
+            int expectedAddedIndex7 = 6;
+            int expectedAddedIndex8 = 7;
+            int expectedAddedIndex9 = 8;
+            int expectedAddedIndex10 = 9;
+            int expectedAddedIndex11 = 10;
+            int expectedAddedIndex12 = 11;
+            int expectedAddedIndex13 = 12;
+            int expectedAddedIndex14 = 13;
+            int expectedAddedIndex15 = 14;
 
             int expectedIndexFull = -1;
             int expectedIndexDuplicate = 10;
@@ -166,24 +166,96 @@ namespace AlgorithmsDataStructures2.Tests
             Assert.IsTrue(testTree.Tree[13] == 13);
             Assert.IsTrue(testTree.Tree[14] == 15);
 
-            Assert.AreEqual(expectedIndex1, index1);
-            Assert.AreEqual(expectedIndex2, index2);
-            Assert.AreEqual(expectedIndex3, index3);
-            Assert.AreEqual(expectedIndex4, index4);
-            Assert.AreEqual(expectedIndex5, index5);
-            Assert.AreEqual(expectedIndex6, index6);
-            Assert.AreEqual(expectedIndex7, index7);
-            Assert.AreEqual(expectedIndex8, index8);
-            Assert.AreEqual(expectedIndex9, index9);
-            Assert.AreEqual(expectedIndex10, index10);
-            Assert.AreEqual(expectedIndex11, index11);
-            Assert.AreEqual(expectedIndex12, index12);
-            Assert.AreEqual(expectedIndex13, index13);
-            Assert.AreEqual(expectedIndex14, index14);
-            Assert.AreEqual(expectedIndex15, index15);
+            Assert.AreEqual(expectedAddedIndex1, index1);
+            Assert.AreEqual(expectedAddedIndex2, index2);
+            Assert.AreEqual(expectedAddedIndex3, index3);
+            Assert.AreEqual(expectedAddedIndex4, index4);
+            Assert.AreEqual(expectedAddedIndex5, index5);
+            Assert.AreEqual(expectedAddedIndex6, index6);
+            Assert.AreEqual(expectedAddedIndex7, index7);
+            Assert.AreEqual(expectedAddedIndex8, index8);
+            Assert.AreEqual(expectedAddedIndex9, index9);
+            Assert.AreEqual(expectedAddedIndex10, index10);
+            Assert.AreEqual(expectedAddedIndex11, index11);
+            Assert.AreEqual(expectedAddedIndex12, index12);
+            Assert.AreEqual(expectedAddedIndex13, index13);
+            Assert.AreEqual(expectedAddedIndex14, index14);
+            Assert.AreEqual(expectedAddedIndex15, index15);
 
             Assert.AreEqual(expectedIndexFull, indexFull);
             Assert.AreEqual(expectedIndexDuplicate, indexDuplicate);
+        }
+
+        [TestMethod()]
+        public void FindKey_If_4_Level_Tree_has_Empty_4_Level()
+        {
+            aBST testTree = new aBST(4);
+            int index1 = testTree.AddKey(8);
+            int index2 = testTree.AddKey(4);
+            int index3 = testTree.AddKey(12);
+            int index4 = testTree.AddKey(2);
+            int index5 = testTree.AddKey(6);
+            int index6 = testTree.AddKey(10);
+            int index7 = testTree.AddKey(14);
+
+            int? index8 = testTree.FindKeyIndex(1);
+            int? index9 = testTree.FindKeyIndex(3);
+            int? index10 = testTree.FindKeyIndex(5);
+            int? index11 = testTree.FindKeyIndex(7);
+            int? index12 = testTree.FindKeyIndex(9);
+            int? index13 = testTree.FindKeyIndex(11);
+            int? index14 = testTree.FindKeyIndex(13);
+            int? index15 = testTree.FindKeyIndex(15);
+                        
+            int expectedAddedIndex1 = 0;
+            int expectedAddedIndex2 = 1;
+            int expectedAddedIndex3 = 2;
+            int expectedAddedIndex4 = 3;
+            int expectedAddedIndex5 = 4;
+            int expectedAddedIndex6 = 5;
+            int expectedAddedIndex7 = 6;
+            int expectedAddedIndex8 = -7;
+            int expectedAddedIndex9 = -8;
+            int expectedAddedIndex10 = -9;
+            int expectedAddedIndex11 = -10;
+            int expectedAddedIndex12 = -11;
+            int expectedAddedIndex13 = -12;
+            int expectedAddedIndex14 = -13;
+            int expectedAddedIndex15 = -14;
+
+            Assert.IsTrue(testTree.Tree.Length == 15);
+            Assert.IsTrue(testTree.Tree[0] == 8);
+            Assert.IsTrue(testTree.Tree[1] == 4);
+            Assert.IsTrue(testTree.Tree[2] == 12);
+            Assert.IsTrue(testTree.Tree[3] == 2);
+            Assert.IsTrue(testTree.Tree[4] == 6);
+            Assert.IsTrue(testTree.Tree[5] == 10);
+            Assert.IsTrue(testTree.Tree[6] == 14);
+
+            Assert.IsNull(testTree.Tree[7]);
+            Assert.IsNull(testTree.Tree[8]);
+            Assert.IsNull(testTree.Tree[9]);
+            Assert.IsNull(testTree.Tree[10]);
+            Assert.IsNull(testTree.Tree[11]);
+            Assert.IsNull(testTree.Tree[12]);
+            Assert.IsNull(testTree.Tree[13]);
+            Assert.IsNull(testTree.Tree[14]);
+
+            Assert.AreEqual(expectedAddedIndex1, index1);
+            Assert.AreEqual(expectedAddedIndex2, index2);
+            Assert.AreEqual(expectedAddedIndex3, index3);
+            Assert.AreEqual(expectedAddedIndex4, index4);
+            Assert.AreEqual(expectedAddedIndex5, index5);
+            Assert.AreEqual(expectedAddedIndex6, index6);
+            Assert.AreEqual(expectedAddedIndex7, index7);
+            Assert.AreEqual(expectedAddedIndex8, index8);
+            Assert.AreEqual(expectedAddedIndex9, index9);
+            Assert.AreEqual(expectedAddedIndex10, index10);
+            Assert.AreEqual(expectedAddedIndex11, index11);
+            Assert.AreEqual(expectedAddedIndex12, index12);
+            Assert.AreEqual(expectedAddedIndex13, index13);
+            Assert.AreEqual(expectedAddedIndex14, index14);
+            Assert.AreEqual(expectedAddedIndex15, index15);
         }
     }
 }
