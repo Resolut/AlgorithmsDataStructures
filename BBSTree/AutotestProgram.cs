86edd8ac9c68ee34dfb2bb85b975ee8e371618c7
Dmitriy Samofalov <samdimus@gmail.com>
2019-04-22 18:35:25 +0300

    добавлен черновой код в метод  GenerateBBSTArray

---
+++ BBSTree/AutotestProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            int[] left = new int[a.Length];
+            int[] right = new int[a.Length];
+            Array.Copy(a, 0, left, 0, a.Length / 2); // левая часть
+            // Array.Copy(a, a.Length / 2 + 1, right, a.Length / 2 + 1, (a.Length-1) - (a.Length / 2)); // правая часть
+            return left;
+            
+        }
+    }
+}
