358e469993c0b8f182591995ac445a201285ca37
Dmitriy Samofalov <samdimus@gmail.com>
2019-04-23 13:07:31 +0300

    предварительный код для метода GenerateBBST

--- BBSTree/AutotestProgram.cs
+++ BBSTree/AutotestProgram.cs
@@ -7,12 +7,23 @@ namespace AlgorithmsDataStructures2
     {
         public static int[] GenerateBBSTArray(int[] a)
         {
-            int[] left = new int[a.Length];
-            int[] right = new int[a.Length];
-            Array.Copy(a, 0, left, 0, a.Length / 2); // левая часть
-            // Array.Copy(a, a.Length / 2 + 1, right, a.Length / 2 + 1, (a.Length-1) - (a.Length / 2)); // правая часть
-            return left;
+            int[] arrBST = new int[a.Length]; // результирующий массив со структурой BBST
+            int index = 0;
+            int middleIndex = a.Length / 2;                                 // индекс центрального элемента массива
+            arrBST[index] = a[middleIndex];                                 // корень дерева
+            arrBST[2 * index + 1] = a[middleIndex / 2];                     // сюда вернуть левого потомка с помощью рекурсии
+            arrBST[2 * index + 2] = a[middleIndex + middleIndex / 2 + 1];   // сюда вернуть правого потомка с помощью рекурсии
+            // нужно как-то узнать индекс следующего узла
+
+            return arrBST; 
             
+        }
+        // TODO возможно, потребуется создать вспомогательную рекурсивную функцию 
+        private static int Recursive(int[] arr)
+        {
+            // выход из рекурсии : индекс превысил длину массива??
+
+            return 0;
         }
     }
 }
