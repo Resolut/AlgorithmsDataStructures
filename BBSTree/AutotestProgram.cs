e04f13c7dc3c5ae27e0394e8010d16489e6a2310
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-05 15:07:17 +0300

    метод GenerateBBSTArray возвращает массив со структурой сбалансированного дерева; изменен код рекурсивного метода Add;

--- BBSTree/AutotestProgram.cs
+++ BBSTree/AutotestProgram.cs
@@ -10,40 +10,30 @@ namespace AlgorithmsDataStructures2
         public static int[] GenerateBBSTArray(int[] a)
         {
             arrayBST = new int[a.Length];
-            Array.Sort(a);
-            // Вызвать рекурсивный метод, в котором происходит заполнение массива
-            Add(a, 0);
-            return a;
+            Array.Sort(a);  // сортируем исходный массив по возрастанию
+            Add(a, 0);      // формируем массив со структурой сбалансированного дерева
 
+            return arrayBST;
         }
-        // TODO возможно, потребуется создать вспомогательную рекурсивную функцию 
+        
         private static void Add(int[] arr, int index)
         {
-            // выход из рекурсии : индекс превысил длину массива??
-            
-            int middle = arr.Length / 2;
-            arrayBST[index] = arr[middle];
-            int[] left = new int[arr.Length / 2];
-            int[] right = new int [arr.Length / 2];
-            Console.WriteLine("Левый массив:");
-            foreach (var item in left)
+            int middle = arr.Length / 2;        // центральный индекс массива
+            arrayBST[index] = arr[middle];      // центральный элемент - корень дерева 
+
+            if (arr.Length == 1) return;        // выход из рекурсии
+
+            int[] left = new int[middle];       // левый подмассив
+            int[] right = new int [middle];     // правый подмассив
+
+            for (int i = 0; i < middle; i++)
             {
-
-            Console.Write(item + " ");
+                left[i] = arr[i];               // вычисление левой части массива
+                right[i] = arr[middle + i + 1]; // вычисление правой части массива
             }
-            Console.WriteLine();
-            arr.CopyTo(right, 0);
 
-            Console.WriteLine("Правый массив:");
-            foreach (var item in right)
-            {
-
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-
-            //Add(left, 2 * index + 1);                     // сюда вернуть левого потомка с помощью рекурсии
-            //Add(right, 2 * index + 2);
+            Add(left, 2 * index + 1);           // определяем левого наследника родительского узла
+            Add(right, 2 * index + 2);          // определяем правого наследника родительского узла 
 
         }
     }
