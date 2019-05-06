3a8d064568309ba16497e8982bfb72f4b5518eb0
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-06 16:25:41 +0300

    некоторые проекты переименованы

--- ABSTree/Properties/AssemblyInfo.cs
+++ ABSTree/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ABSTree")]
+[assembly: AssemblyTitle("ABST")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ABSTree")]
+[assembly: AssemblyProduct("ABST")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
