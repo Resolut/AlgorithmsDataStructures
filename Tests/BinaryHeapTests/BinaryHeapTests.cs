9c9b38de00907ad4b3fb0fd4e7ac470227a88cec
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-20 18:54:11 +0300

    Добавлен тест построения 3 уровневой пирамиды

---
+++ Tests/BinaryHeapTests/BinaryHeapTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AlgorithmsDataStructures2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryHeapTests
+{
+    [TestClass]
+    public class BinaryHeapTests
+    {
+        [TestMethod]
+        public void Add_with_Root_only()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 6, 5, 2, 3, 1, 7, 4 }, 2);
+            
+
+            Assert.AreEqual(heap.HeapArray[0], 7);
+        }
+    }
+}
---
+++ Tests/BinaryHeapTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinaryHeapTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinaryHeapTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6a304a9-1e50-4adc-8307-b9d6bfcc2458")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
