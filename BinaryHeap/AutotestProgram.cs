eb64d8a92d11940b87002b307fac06a790982eaa
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-20 18:48:50 +0300

    добавлен код методов MakeHeap, Add

--- BinaryHeap/AutotestProgram.cs
+++ BinaryHeap/AutotestProgram.cs
@@ -12,21 +12,42 @@ namespace AlgorithmsDataStructures2
 
         public void MakeHeap(int[] a, int depth)
         {
-            // создаём массив кучи HeapArray из заданного
-            // размер массива выбираем на основе глубины depth
-            // ...
+            int tree_size = (int)Math.Pow(2, depth + 1) - 1;
+            HeapArray = new int[tree_size];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+            Add(a[i]);
+            }
         }
 
         public int GetMax()
         {
+            // TODO реализовать перестроение кучи
             // вернуть значение корня и перестроить кучу
             return -1; // если куча пуста
         }
 
         public bool Add(int key)
         {
-            // добавляем новый элемент key в кучу и перестраиваем её
-            return false; // если куча вся заполнена
+            int index = Array.IndexOf(HeapArray, 0);    // первый свободный слот
+            if (index == -1) return false;              // вся куча заполнена
+
+            HeapArray[index] = key;
+            int parent = (index - 1) / 2;              // индекс родительского узла
+
+            while (index > 0 && parent >= 0)
+            {
+                if (HeapArray[index] > HeapArray[parent]) {
+                    int temp = HeapArray[index];
+                    HeapArray[index] = HeapArray[parent];
+                    HeapArray[parent] = temp;
+                }
+                index = parent;
+                parent = (index - 1) / 2;
+            }
+
+            return true;
         }
 
     }
