b1e5e7ce863a747efd29427fc5b6987c695bbe1d
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-21 18:44:12 +0300

    добавлены тесты для проверки метода GetMax()

--- Tests/BinaryHeapTests/BinaryHeapTests.cs
+++ Tests/BinaryHeapTests/BinaryHeapTests.cs
@@ -12,9 +12,41 @@ namespace BinaryHeapTests
         {
             Heap heap = new Heap();
             heap.MakeHeap(new int[] { 6, 5, 2, 3, 1, 7, 4 }, 2);
-            
 
             Assert.AreEqual(heap.HeapArray[0], 7);
+        }
+
+        [TestMethod]
+        public void GetMax_3_level_Tree()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 6, 5, 2, 3, 1, 7, 4 }, 2);
+            int maxKey = heap.GetMax();
+            foreach (var item in heap.HeapArray)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Assert.AreEqual(maxKey, 7);
+        }
+
+        [TestMethod]
+        public void GetMax_3_level_Tree_Another_InArr()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 8, 3, 2, 1, 16, 9, 7 }, 2);
+            foreach (var item in heap.HeapArray)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            int maxKey = heap.GetMax();
+            foreach (var item in heap.HeapArray)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Assert.AreEqual(maxKey, 16);
         }
     }
 }
