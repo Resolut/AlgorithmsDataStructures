079363f226e07e12eccb335d90823839a7b758ad
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-22 11:15:42 +0300

    добавлено поле HeapSize; добавлен рекурсивный метод SiftDown; рефакторинг методов Add, GetMax;

--- BinaryHeap/AutotestProgram.cs
+++ BinaryHeap/AutotestProgram.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures2
 {
@@ -7,7 +6,7 @@ namespace AlgorithmsDataStructures2
     {
 
         public int[] HeapArray; // хранит неотрицательные числа-ключи
-
+        public int HeapSize;    // размер кучи
         public Heap() { HeapArray = null; }
 
         public void MakeHeap(int[] a, int depth)
@@ -15,49 +14,35 @@ namespace AlgorithmsDataStructures2
             int tree_size = (int)Math.Pow(2, depth + 1) - 1;
             HeapArray = new int[tree_size];
 
-            for (int i = 0; i < a.Length; i++) { Add(a[i]); }
+            for (int i = 0; i < a.Length; i++) Add(a[i]); // перестроение кучи с каждым добавлением ключа 
         }
 
         public int GetMax()
         {
-            if (HeapArray == null || HeapArray[0] == 0) return -1; // если куча пуста
+            if (HeapArray == null || HeapSize == 0) return -1; // если куча пуста
 
             int maxKey = HeapArray[0];
-            int lastIndex = Array.FindLastIndex(HeapArray, (item) => item > 0); // индекс крайнего элемента
-            HeapArray[0] = HeapArray[lastIndex];
-            HeapArray[lastIndex] = 0;
 
-            int index = 0;
-            int maxChildIndex = HeapArray[index * 2 + 1] > HeapArray[index * 2 + 2] ? (index * 2 + 1) : (index * 2 + 2);
-
-            while (index < lastIndex - 1 && maxChildIndex <= lastIndex)
-            {
-                if (HeapArray[index] < HeapArray[maxChildIndex])
-                {
-                    int temp = HeapArray[index];
-                    HeapArray[index] = HeapArray[maxChildIndex];
-                    HeapArray[maxChildIndex] = temp;
-                }
-
-                index = maxChildIndex;
-                if ((index * 2 + 2) <= lastIndex )
-                maxChildIndex = HeapArray[index * 2 + 1] > HeapArray[index * 2 + 2] ? (index * 2 + 1) : (index * 2 + 2);
-            }
+            HeapArray[0] = HeapArray[HeapSize - 1];
+            HeapArray[HeapSize - 1] = 0;
+            HeapSize--;
+            SiftDown(0); // перестроение дерева
 
             return maxKey;
         }
 
         public bool Add(int key)
         {
-            int index = Array.IndexOf(HeapArray, 0);    // первый свободный слот
-            if (index == -1) return false;              // вся куча заполнена
+            if (HeapSize == HeapArray.Length) return false; // если вся куча заполнена
 
+            int index = HeapSize;                           // индекс первого свободного слота
             HeapArray[index] = key;
             int parentIndex = (index - 1) / 2;              // индекс родительского узла
 
             while (index > 0 && parentIndex >= 0)
             {
-                if (HeapArray[index] > HeapArray[parentIndex]) {
+                if (HeapArray[index] > HeapArray[parentIndex])
+                {
                     int temp = HeapArray[index];
                     HeapArray[index] = HeapArray[parentIndex];
                     HeapArray[parentIndex] = temp;
@@ -66,9 +51,37 @@ namespace AlgorithmsDataStructures2
                 index = parentIndex;
                 parentIndex = (index - 1) / 2;
             }
+            HeapSize++;
 
             return true;
         }
+        private void SiftDown(int index)
+        {
+            int left = index * 2 + 1;
+            int right = index * 2 + 2;
 
+            if (left < HeapSize)
+            {
+                if (HeapArray[index] < HeapArray[left])
+                {
+                    int temp = HeapArray[index];
+                    HeapArray[index] = HeapArray[left];
+                    HeapArray[left] = temp;
+
+                    SiftDown(left);
+                }
+            }
+            if (right < HeapSize)
+            {
+                if (HeapArray[index] < HeapArray[right])
+                {
+                    int temp = HeapArray[index];
+                    HeapArray[index] = HeapArray[right];
+                    HeapArray[right] = temp;
+
+                    SiftDown(right);
+                }
+            }
+        }
     }
 }
