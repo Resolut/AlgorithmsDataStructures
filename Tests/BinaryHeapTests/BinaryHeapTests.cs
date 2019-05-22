d0fa41c43b9875cf518d817070f2d78f9844716b
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-22 11:13:59 +0300

    добавлены тесты

--- Tests/BinaryHeapTests/BinaryHeapTests.cs
+++ Tests/BinaryHeapTests/BinaryHeapTests.cs
@@ -8,20 +8,32 @@ namespace BinaryHeapTests
     public class BinaryHeapTests
     {
         [TestMethod]
-        public void Add_with_Root_only()
+        public void MakeHeap_3_level_Heap()
         {
             Heap heap = new Heap();
             heap.MakeHeap(new int[] { 6, 5, 2, 3, 1, 7, 4 }, 2);
 
             Assert.AreEqual(heap.HeapArray[0], 7);
+            Assert.IsTrue(heap.HeapSize == 7);
         }
 
         [TestMethod]
-        public void GetMax_3_level_Tree()
+        public void MakeHeap_if_Root_Only()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 19 }, 0);
+
+            Assert.AreEqual(heap.HeapArray[0], 19);
+            Assert.IsTrue(heap.HeapSize == 1);
+        }
+
+        [TestMethod]
+        public void GetMax_3_level_Heap()
         {
             Heap heap = new Heap();
             heap.MakeHeap(new int[] { 6, 5, 2, 3, 1, 7, 4 }, 2);
             int maxKey = heap.GetMax();
+
             foreach (var item in heap.HeapArray)
             {
                 Console.Write("{0} ", item);
@@ -31,7 +43,7 @@ namespace BinaryHeapTests
         }
 
         [TestMethod]
-        public void GetMax_3_level_Tree_Another_InArr()
+        public void GetMax_3_level_Heap_Another_InArr()
         {
             Heap heap = new Heap();
             heap.MakeHeap(new int[] { 8, 3, 2, 1, 16, 9, 7 }, 2);
@@ -40,7 +52,9 @@ namespace BinaryHeapTests
                 Console.Write("{0} ", item);
             }
             Console.WriteLine();
+
             int maxKey = heap.GetMax();
+
             foreach (var item in heap.HeapArray)
             {
                 Console.Write("{0} ", item);
@@ -49,40 +63,67 @@ namespace BinaryHeapTests
             Assert.AreEqual(maxKey, 16);
         }
         [TestMethod]
-        public void GetMax_2_level_Tree_Another_InArr()
+        public void GetMax_2_level_Heap()
         {
             Heap heap = new Heap();
             heap.MakeHeap(new int[] { 1, 16, 8 }, 1);
-            foreach (var item in heap.HeapArray)
-            {
-                Console.Write("{0} ", item);
-            }
-            Console.WriteLine();
-            int maxKey = heap.GetMax();
+
             foreach (var item in heap.HeapArray)
             {
                 Console.Write("{0} ", item);
             }
             Console.WriteLine();
 
+            int maxKey = heap.GetMax();
             int maxKey2 = heap.GetMax();
-            foreach (var item in heap.HeapArray)
-            {
-                Console.Write("{0} ", item);
-            }
-            Console.WriteLine();
             int maxKey3 = heap.GetMax();
-            foreach (var item in heap.HeapArray)
-            {
-                Console.Write("{0} ", item);
-            }
-            Console.WriteLine();
             int maxKey4 = heap.GetMax();
 
             Assert.AreEqual(maxKey, 16);
             Assert.AreEqual(maxKey2, 8);
             Assert.AreEqual(maxKey3, 1);
             Assert.AreEqual(maxKey4, -1);
+        }
+
+        [TestMethod]
+        public void GetMax_if_Root_Only()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 19 }, 0);
+
+            Assert.AreEqual(heap.HeapArray[0], 19);
+            Assert.IsTrue(heap.HeapSize == 1);
+
+            int maxKey = heap.GetMax();
+            Assert.AreEqual(maxKey, 19);
+            Assert.AreEqual(heap.HeapArray[0], 0);
+            Assert.IsTrue(heap.HeapSize == 0);
+        }
+
+        [TestMethod]
+        public void GetMax_if_Heap_is_Empty()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 19 }, 0);
+
+            int maxKey = heap.GetMax();
+            int maxFromEmptyHeap = heap.GetMax();
+
+            Assert.AreEqual(maxKey, 19);
+            Assert.AreEqual(maxFromEmptyHeap, -1);
+            Assert.IsTrue(heap.HeapSize == 0);
+        }
+
+        [TestMethod]
+        public void Add_if_Heap_is_Full()
+        {
+            Heap heap = new Heap();
+            heap.MakeHeap(new int[] { 6, 5, 2, 3, 1, 7, 4 }, 2);
+            bool isAdded = heap.Add(170);
+
+            Assert.AreEqual(heap.HeapArray[0], 7);
+            Assert.IsTrue(heap.HeapSize == 7);
+            Assert.IsFalse(isAdded);
         }
     }
 }
