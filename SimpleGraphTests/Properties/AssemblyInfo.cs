151b8d732666901c0438b7b05c1fc7182c70a7f1
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-28 18:52:15 +0300

    добавлен тест-заглушка метода  AddVertex

---
+++ SimpleGraphTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleGraphTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleGraphTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e6f3140-7534-4545-b96b-cda9fed048c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleGraphTests/SimpleGraphTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AlgorithmsDataStructures2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleGraphTests
+{
+    [TestClass]
+    public class SimpleGraphTests
+    {
+        [TestMethod]
+        public void AddVertex_5_Graph()
+        {
+            Assert.Fail();
+        }
+    }
+}
