0aef0d4e83ccc57c020527fd82755ba99da3f91b
Dmitriy Samofalov <samdimus@gmail.com>
2020-07-08 20:09:49 +0300

    Рефакторинг тестов. Добавлены тесты метода InsertionSortStep

--- Tests/BaseSortingTypesTests/SortLevelTests.cs
+++ Tests/BaseSortingTypesTests/SortLevelTests.cs
@@ -22,7 +22,7 @@ namespace SortSpace.Tests
 
             for (int i = 0; i < testArr.Length; i++)
             {
-                Assert.AreEqual(testArr[i], expectedSortedArr[i]);
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
             }
         }
 
@@ -68,7 +68,7 @@ namespace SortSpace.Tests
 
             for (int i = 0; i < testArr.Length; i++)
             {
-                Assert.AreEqual(testArr[i], expectedSortedArr[i]);
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
             }
 
         }
@@ -91,7 +91,7 @@ namespace SortSpace.Tests
 
             for (int i = 0; i < testArr.Length; i++)
             {
-                Assert.AreEqual(testArr[i], expectedSortedArr[i]);
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
             }
         }
 
@@ -142,8 +142,172 @@ namespace SortSpace.Tests
 
             for (int i = 0; i < testArr.Length; i++)
             {
-                Assert.AreEqual(testArr[i], expectedSortedArr[i]);
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
             }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStepTest_if_Array_Has_3_Elements()
+        {
+            int[] testArr = { 3, 4, 2 };
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            SortLevel.InsertionSortStep(testArr, 1, 0);
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] { 2, 3, 4 };
+            
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStepTest_if_Array_Has_7_Elements()
+        {
+            int[] testArr = { 7, 6, 5, 4, 3, 2, 1 };
+            
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            SortLevel.InsertionSortStep(testArr, 1, 0);
+
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] {1, 2, 3, 4, 5, 6, 7};
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStepTest_if_Array_Has_Step_3()
+        {
+            int[] testArr = { 7, 6, 5, 4, 3, 2, 1 };
+
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            SortLevel.InsertionSortStep(testArr, 3, 0);
+            
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] { 1, 6, 5, 4, 3, 2, 7 };
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStepTest_if_Array_Has_Step_2()
+        {
+            int[] testArr = { 7, 6, 5, 4, 3, 2, 1 };
+            
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            int step = 2;
+            int startIndex = 0;
+            
+            SortLevel.InsertionSortStep(testArr, step, startIndex);
+            
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] { 1, 6, 3, 4, 5, 2, 7 };
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStepTest_if_Array_Has_Step_3_startIndex_1()
+        {
+            int[] testArr = { 1, 6, 5, 4, 3, 2, 7 };
+            
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            int step = 3;
+            int startIndex = 1;
+            
+            SortLevel.InsertionSortStep(testArr, step, startIndex);
+            
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] { 1, 3, 5, 4, 6, 2, 7 };
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStepTest_if_Array_Has_Step_4_startIndex_0()
+        {
+            int[] testArr = { 6, 2, 5, 4, 1, 2, 7 };
+
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            int step = 4;
+            int startIndex = 0;
+
+            SortLevel.InsertionSortStep(testArr, step, startIndex);
+
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] { 1, 2, 5, 4, 6, 2, 7 };
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(expectedSortedArr[i], testArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortStep_if_Array_is_Empty()
+        {
+            int[] testArr = { };
+
+            SortLevel.InsertionSortStep(testArr, 5, 0);
+
+            Assert.IsNotNull(testArr);
+            Assert.IsTrue(testArr.Length == 0);
+        }
+
+        [TestMethod()]
+        public void InsertionSortStep_if_Array_Has_1_Elem()
+        {
+            int[] testArr = { 19 };
+
+            SortLevel.InsertionSortStep(testArr, 12, 0);
+
+            int expected = 19;
+
+            Assert.IsNotNull(testArr);
+            Assert.IsTrue(testArr.Length == 1);
+            Assert.AreEqual(expected, testArr[0]);
         }
     }
 }
