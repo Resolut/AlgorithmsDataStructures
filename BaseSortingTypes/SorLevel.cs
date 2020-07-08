9ab93e5aa6060cfa90cda10d739ea0889270989f
Dmitriy Samofalov <samdimus@gmail.com>
2020-07-08 11:27:28 +0300

    добавлен начальный шаблон статического класса SortLevel

---
+++ BaseSortingTypes/SorLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public static class SortLevel
+    {
+        public static void SelectionSortStep(ref int[] array, int i)
+        {
+
+        }
+
+        public static bool BubbleSortStep(ref int[] array)
+        {
+            return false;
+        }
+    }
+}
