b1998f0a8a187f93735bf0b5bff00b02b66b14f4
Dmitriy Samofalov <samdimus@gmail.com>
2020-07-08 14:42:52 +0300

    Добавлены тесты метода BubbleSortStep

--- Tests/BaseSortingTypesTests/SortLevelTests.cs
+++ Tests/BaseSortingTypesTests/SortLevelTests.cs
@@ -72,5 +72,78 @@ namespace SortSpace.Tests
             }
 
         }
+
+        [TestMethod()]
+        public void BubbleSortStepTest_if_Array_unsorted()
+        {
+            int[] testArr = { 4, 3, 1, 2 };
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            bool res = SortLevel.BubbleSortStep(ref testArr);
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] {3, 1, 2, 4 };
+            
+            Assert.IsFalse(res);
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(testArr[i], expectedSortedArr[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void BubbleSortStep_if_Array_is_Empty()
+        {
+            int[] testArr = { };
+
+            bool res = SortLevel.BubbleSortStep(ref testArr);
+
+            Assert.IsNotNull(testArr);
+            Assert.IsTrue(res);
+            Assert.IsTrue(testArr.Length == 0);
+        }
+
+        [TestMethod()]
+        public void BubbleSortStep_if_Array_Has_1_Elem()
+        {
+            int[] testArr = { 19 };
+
+            bool res = SortLevel.BubbleSortStep(ref testArr);
+
+            int expected = 19;
+
+            Assert.IsNotNull(testArr);
+            Assert.IsTrue(res);
+            Assert.IsTrue(testArr.Length == 1);
+            Assert.AreEqual(expected, testArr[0]);
+        }
+
+        [TestMethod()]
+        public void BubbleSortStepTest_if_Array_is_Already_Sorted()
+        {
+            int[] testArr = { 1, 2, 3, 4 };
+            Array.ForEach(testArr, (item) => Console.Write(item + " "));
+            Console.WriteLine();
+
+            bool res = SortLevel.BubbleSortStep(ref testArr);
+            foreach (int item in testArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int[] expectedSortedArr = new int[] { 1, 2, 3, 4 };
+
+            Assert.IsNotNull(testArr);
+            Assert.IsTrue(res);
+            Assert.IsTrue(testArr.Length == 4);
+
+            for (int i = 0; i < testArr.Length; i++)
+            {
+                Assert.AreEqual(testArr[i], expectedSortedArr[i]);
+            }
+        }
     }
 }
