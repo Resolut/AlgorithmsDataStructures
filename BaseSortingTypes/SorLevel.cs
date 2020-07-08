16045df15aef21e6c24d6ecdaa46c097e117ec8c
Dmitriy Samofalov <samdimus@gmail.com>
2020-07-08 12:32:14 +0300

    Добавлен код метода SelectionSortStep

--- BaseSortingTypes/SorLevel.cs
+++ BaseSortingTypes/SorLevel.cs
@@ -7,7 +7,23 @@ namespace SortSpace
     {
         public static void SelectionSortStep(ref int[] array, int i)
         {
+            if (array.Length <= 1) return;
 
+            int minIndex = i + 1;
+            for (int j = i + 1; j < array.Length; ++j)
+            {
+                if (array[j] < array[minIndex]) 
+                { 
+                    minIndex = j;
+                }
+            }
+
+            if (array[i] > array[minIndex]) 
+            {
+                int tmp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = tmp;
+            }
         }
 
         public static bool BubbleSortStep(ref int[] array)
