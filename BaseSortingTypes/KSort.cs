4e91009ef93892514a458287f59c81ba2cb92c2a
Dmitriy Samofalov <samdimus@gmail.com>
2020-08-19 18:06:47 +0300

    добавлен класс KSort с двумя методами Add и Index

---
+++ BaseSortingTypes/KSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortSpace
+{
+    class KSort
+    {
+        string[] items;
+        public KSort() 
+        {
+            int maxSize = 800;
+            items = new string[maxSize];
+        }
+
+        public int Index(string str) 
+        {
+            string pattern = "abcdefgh";
+            int bigDigit; 
+            
+            if (pattern.Contains(str[0]))
+                bigDigit = pattern.IndexOf(str[0]) * 100;
+            else 
+                return -1;
+
+            int midDigit;
+
+            if (!int.TryParse(str[1].ToString(), out midDigit))
+                return -1;
+
+            midDigit *= 10;
+
+            int smallDigit;
+
+            if (!int.TryParse(str[2].ToString(), out smallDigit))
+                return -1;
+
+            return bigDigit + midDigit + smallDigit;
+        }
+
+        public bool Add(string str)
+        {
+            int index = Index(str);
+            if (index == -1)
+                return false;
+
+            items[index] = str;
+
+            return true;
+        }
+    }
+}
