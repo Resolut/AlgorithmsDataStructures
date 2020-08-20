c0d3377f895806c7b5114b22ded096e8421768d6
Dmitriy Samofalov <samdimus@gmail.com>
2020-08-20 10:23:43 +0300

    Добавлен класс BinarySearch с методами GetResult Step и полями Left и Right

---
+++ BaseSortingTypes/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortSpace
+{
+    public class BinarySearch
+    {
+        public int Left;
+        public int Right;
+        public BinarySearch(int[] array) 
+        {
+        }
+
+        public int GetResult() 
+        {
+            return 0;
+        }
+
+        public void Step(int N) 
+        {
+        
+        }
+    }
+}
